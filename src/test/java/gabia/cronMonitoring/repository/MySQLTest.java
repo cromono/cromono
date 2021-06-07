acf18db1a2567c5aecea9c45fcd8ef4dd1fbaff3
Joon(주영준) <GM2202985>
2021-06-07 16:02:21 +0900

    test: Test 코드 보완M

--- src/test/java/gabia/cronMonitoring/repository/MySQLTest.java
+++ src/test/java/gabia/cronMonitoring/repository/MySQLTest.java
@@ -19,22 +19,40 @@ public class MySQLTest {
     @Autowired
     MemberRepository memberRepository;
 
-    //MySQL 접속 Test 밑 Jpa Test
+    //MySQL 접속 Test 및 Jpa Test
     @Test
     @Transactional
     public void testMember() throws Exception {
         //given
         Member member = new Member();
+        Member member2 = new Member();
+        Member member3 = new Member();
+
         member.setUsername("MySQL Test");
+        member2.setUsername("MySQL Test2");
+        member3.setUsername("MySQL Test3");
 
         //when
         Long saveId = memberRepository.save(member);
+        Long saveId2 = memberRepository.save(member2);
+        Long saveId3 = memberRepository.save(member3);
+
         Member findMember = memberRepository.find(saveId);
+        Member findMember2 = memberRepository.find(saveId2);
+        Member findMember3 = memberRepository.find(saveId3);
 
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(saveId);
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat((findMember)).isEqualTo(member);
+
+        Assertions.assertThat(findMember2.getId()).isEqualTo(saveId2);
+        Assertions.assertThat(findMember2.getUsername()).isEqualTo(member2.getUsername());
+        Assertions.assertThat((findMember2)).isEqualTo(member2);
+
+        Assertions.assertThat(findMember3.getId()).isEqualTo(saveId3);
+        Assertions.assertThat(findMember3.getUsername()).isEqualTo(member3.getUsername());
+        Assertions.assertThat((findMember3)).isEqualTo(member3);
     }
 
 }
