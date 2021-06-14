2932ad6daffc23238bab61ffcec09ab3ea79e7b7
GM2202983 <yhw@gabia.com>
2021-06-14 11:48:33 +0900

    feat: 알림관련 엔티티 개발

--- src/main/java/gabia/cronMonitoring/entity/Notice.java
+++ src/main/java/gabia/cronMonitoring/entity/Notice.java
@@ -1,5 +1,56 @@
 package gabia.cronMonitoring.entity;
 
+import gabia.cronMonitoring.entity.Enum.NoticeType;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table( name = "notice")
 public class Notice {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="not_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="userId")
+
+    private User receiveUserId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="userId")
+    private User createUserId;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private NoticeType noticeType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="id")
+    private CronJob cronJob;
+
+    @NotNull
+    private String noticeMessage;
+
+    @NotNull
+    private LocalDateTime noticeCreateDatetime;
+
+    @NotNull
+    private LocalDateTime noticeReadDatetime;
 
 }
--- src/main/java/gabia/cronMonitoring/entity/Webhook.java
+++ src/main/java/gabia/cronMonitoring/entity/Webhook.java
@@ -1,5 +1,31 @@
 package gabia.cronMonitoring.entity;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader.Strategy;
+
+@Entity
+@Table(name = "webhook")
+@Getter@Setter
 public class Webhook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id")
+    private Long id;
+
+    @NotNull
+    private Long not_id;
+
+    @NotNull
+    private String url;
 
 }
