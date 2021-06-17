ce0d7add817dd709adc112a42a3f7454f6be988a
Luke(김기정) <kkj@gabia.com>
2021-06-17 14:09:09 +0900

    test: 크론 서버 리포지토리 테스트 코드

---
+++ src/test/java/gabia/cronMonitoring/repositoryImpl/CronServerRepositoryImplTest.java
@@ -0,0 +1,72 @@
+package gabia.cronMonitoring.repositoryImpl;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
+
+import gabia.cronMonitoring.entity.CronServer;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CronServerRepositoryImplTest {
+
+    @Autowired
+    CronServerRepositoryImpl cronServerRepository;
+
+    @Test
+    @Transactional
+    public void save() {
+        // Given
+        CronServer cronServer = new CronServer("1:1:1:1");
+        // When
+        CronServer savedServer = cronServerRepository.save(cronServer);
+        // Then
+        assertThat(savedServer).isEqualTo(cronServer);
+    }
+
+    @Test
+    @Transactional
+    public void findByIp() {
+        // Given
+        CronServer cronServer = new CronServer("1:1:1:1");
+        CronServer saveServer = cronServerRepository.save(cronServer);
+        // When
+        CronServer findServer = cronServerRepository.findByIp("1:1:1:1").get();
+        // Then
+        assertThat(findServer).isEqualTo(saveServer);
+    }
+
+    @Test
+    @Transactional
+    public void findAll() {
+        // Given
+        CronServer server1 = new CronServer("1:1:1:1");
+        cronServerRepository.save(server1);
+
+        CronServer server2 = new CronServer("1:1:1:2");
+        cronServerRepository.save(server2);
+        // When
+        List<CronServer> result = cronServerRepository.findAll();
+        // Then
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    @Transactional
+    public void delete() {
+        // Given
+        CronServer cronServer = new CronServer("1:1:1:1");
+        cronServerRepository.save(cronServer);
+        // When
+        cronServerRepository.delete(cronServer);
+        // Then
+//        System.out.println(cronServerRepository.findByIp("1:1:1:1").isNull());
+        assertThat(cronServerRepository.findByIp("1:1:1:1")).isEmpty();
+    }
+}
