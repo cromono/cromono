93c2fea3fd10de7784541ac125e76fbd6b208b25
Joon(주영준) <GM2202985>
2021-06-18 16:24:19 +0900

    feat: CronProcessDto 추가
    - Request, Response, ErrorResponse 포함

---
+++ src/main/java/gabia/cronMonitoring/dto/CronProcessDto.java
@@ -0,0 +1,55 @@
+package gabia.cronMonitoring.dto;
+
+import gabia.cronMonitoring.entity.CronProcess;
+import java.sql.Timestamp;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class CronProcessDto {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Request {
+
+        String pid;
+        Timestamp startTime;
+        Timestamp endTime;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Response {
+
+        UUID cronJobId;
+        String pid;
+        Timestamp startTime;
+        Timestamp endTime;
+
+        public static Response from(CronProcess cronProcess) {
+            return new Response(cronProcess.getCronJob().getId(), cronProcess.getPid(),
+                cronProcess.getStartTime(), cronProcess.getEndTime());
+        }
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class ErrorResponse {
+
+        String errorMsg;
+        String errorCode;
+
+        public ErrorResponse(String errorMsg) {
+            this.errorMsg = errorMsg;
+            this.errorCode = "404";
+        }
+    }
+
+}
