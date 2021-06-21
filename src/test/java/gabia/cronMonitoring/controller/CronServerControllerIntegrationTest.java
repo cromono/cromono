54253ea361165e635ef7c50c5d651d4079d4d3b2
Luke(김기정) <kkj@gabia.com>
2021-06-21 11:58:10 +0900

    test: 크론 서버 컨트롤러 통합테스트

--- src/test/java/gabia/cronMonitoring/controller/CronServerControllerIntegrationTest.java
+++ src/test/java/gabia/cronMonitoring/controller/CronServerControllerIntegrationTest.java
@@ -16,26 +16,42 @@ import gabia.cronMonitoring.dto.CronServerDTO;
 import gabia.cronMonitoring.service.CronServerService;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
+@Transactional
 @RunWith(SpringRunner.class)
-@WebMvcTest(CronServerController.class)
-public class CronServerControllerTest {
+@SpringBootTest
+public class CronServerControllerIntegrationTest {
 
     @Autowired
+    private WebApplicationContext wac;
+
     private MockMvc mockMvc;
 
-    @MockBean
-    private CronServerService cronServerService;
+    @Autowired
+    private CronServerController cronServerController;
 
     ObjectMapper mapper = new ObjectMapper();
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
 
     @Test
     public void 서버_목록_GET() throws Exception {
@@ -43,7 +59,8 @@ public class CronServerControllerTest {
         List<CronServerDTO> servers = new ArrayList<>();
         servers.add(new CronServerDTO("1.1.1.1"));
         servers.add(new CronServerDTO("1.1.1.2"));
-        given(cronServerService.getCronServers()).willReturn(servers);
+        cronServerController.postCronServers(new CronServerDTO("1.1.1.1"));
+        cronServerController.postCronServers(new CronServerDTO("1.1.1.2"));
         // When
         String expectAllServerIp = "$.[*]";
         // Then
@@ -59,7 +76,6 @@ public class CronServerControllerTest {
         // Given
         CronServerDTO cronServerDTO = new CronServerDTO("1.1.1.1");
         String request = mapper.writeValueAsString(cronServerDTO);
-        given(cronServerService.addCronServer(any())).willReturn(cronServerDTO);
         // When
         String expectByServerIp = "$.serverIp";
         // Then
@@ -77,11 +93,11 @@ public class CronServerControllerTest {
         CronServerDTO oldCronServerDto = new CronServerDTO("1.1.1.1");
         CronServerDTO newCronServerDto = new CronServerDTO("1.1.1.2");
         String request = mapper.writeValueAsString(newCronServerDto);
-        given(cronServerService.updateCronServer(any(), any())).willReturn(oldCronServerDto);
+        cronServerController.postCronServers(oldCronServerDto);
         // When
         String expectByServerIp = "$.serverIp";
         // Then
-        mockMvc.perform(patch("/cron-servers/{serverIp}}", "1.1.1.1")
+        mockMvc.perform(patch("/cron-servers/{serverIp}", "1.1.1.1")
             .contentType(MediaType.APPLICATION_JSON)
             .content(request))
             .andDo(print())
@@ -94,6 +110,7 @@ public class CronServerControllerTest {
         // Given
         CronServerDTO cronServerDTO = new CronServerDTO("1.1.1.1");
         String request = mapper.writeValueAsString(cronServerDTO);
+        cronServerController.postCronServers(cronServerDTO);
         // When
         String expectByServerIp = "$.serverIp";
         // Then
