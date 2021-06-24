94b276c7e8a4ef20995f935caf5021dc6dbe8360
Joon(주영준) <GM2202985>
2021-06-24 17:02:44 +0900

    refactor: 코드 리펙토링

--- src/main/java/gabia/cronMonitoring/exception/cron/user/UserNotFoundException.java
+++ src/main/java/gabia/cronMonitoring/exception/cron/user/UserNotFoundException.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class UserNotFoundException extends RuntimeException{
+public class UserNotFoundException extends RuntimeException {
 
     private String message;
 
