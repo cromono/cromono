2d40cdb29605a0e3420ef953aa343d72634ca9bc
Lucas(주영준) <jyj@gabia.com>
2021-07-01 17:02:53 +0900

    feat: Notice Repository 개발
    Resolves: #64

--- src/main/java/gabia/cronMonitoring/repository/NoticeRepository.java
+++ src/main/java/gabia/cronMonitoring/repository/NoticeRepository.java
@@ -1,5 +1,19 @@
 package gabia.cronMonitoring.repository;
 
-public interface NoticeRepository {
+import gabia.cronMonitoring.entity.Notice;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+    Optional<Notice> findById(Long notId);
+
+    List<Notice> findByCronJobIdIn(List<UUID> cronJobIdList);
+
+//    @Query("select n from Notice n where n.cronJob.id in (:ids)")
+//    List<Notice> findByCronJobId(@Param("ids") List<UUID> cronJobIdList);
+
 
 }
