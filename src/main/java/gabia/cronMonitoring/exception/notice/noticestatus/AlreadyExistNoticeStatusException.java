43255b7b019346a8bb9512b9d4973e300dce7ecc
Lucas(주영준) <jyj@gabia.com>
2021-07-01 17:06:39 +0900

    feat: Notice 관련 Exception 추가 및 Handler 추가
    Resolves: #64

---
+++ src/main/java/gabia/cronMonitoring/exception/notice/noticestatus/AlreadyExistNoticeStatusException.java
@@ -0,0 +1,22 @@
+package gabia.cronMonitoring.exception.notice.noticestatus;
+
+import gabia.cronMonitoring.entity.Notice;
+import gabia.cronMonitoring.entity.NoticeStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AlreadyExistNoticeStatusException extends RuntimeException {
+
+    private String message;
+    private Notice notice;
+    public AlreadyExistNoticeStatusException() {
+        this.message = "Already exist notice status";
+    }
+
+    public AlreadyExistNoticeStatusException(NoticeStatus noticeStatus) {
+        this.message = "Already exist notice status";
+        this.notice = noticeStatus.getNotice();
+    }
+}
